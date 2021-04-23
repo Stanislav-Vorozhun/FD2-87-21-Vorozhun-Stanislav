3978a411d3399b191471154c079b0173170ac3e9
Stanislav-Vorozhun <stanislav.vorozhun@gmail.com>
2021-04-23 20:33:38 +0300

    done root

---
+++ js/HashStorageFunc.js
@@ -0,0 +1,56 @@
+class HashStorage {
+    constructor() {
+        this.storage = {};
+    }
+
+    add(key, value) {
+        this.storage[key] = value;
+        console.log('Коктейль успешно добавлен!');
+    }
+
+    deleteValue(key) {
+        if (!(key in this.storage)) {
+            console.log("Такого коктейля нет или название введено неверно.")
+            return false
+        }
+        console.log('Коктейль удален!');
+        return delete this.storage[key];
+
+    }
+
+    getValue(key) {
+        return this.storage[key];
+    }
+
+    getKeys() {
+        return Object.keys(this.storage);
+    }
+}
+
+const coctailsDatabase = new HashStorage();
+
+
+const addButton = document.getElementById('add-coctail');
+addButton.onclick = function() {
+
+    const coctailName = window.prompt('Введите название коктеля');
+    const alcohol = window.prompt('Напиток алкогольный?');
+    const recipe = window.prompt('Напишите рецепт');
+
+    coctailsDatabase.add(coctailName, { coctailName, alcohol, recipe });
+
+}
+
+const delButton = document.getElementById('dell-coctail');
+delButton.onclick = function() {
+    const coctailName = window.prompt('Введите название: ');
+    coctailsDatabase.deleteValue(coctailName);
+
+
+
+}
+
+const listButton = document.getElementById('list-coctail');
+listButton.onclick = function() {
+    console.log(coctailsDatabase.storage);
+}
